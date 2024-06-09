3b7d6fe5a5bb0c336ced96f5c6f70f6e825e7232
Deepankur Sadana <writetodeepankursadana@gmail.com>
2024-06-09 23:33:27 +0530

    ..

--- app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/recycle.kt
+++ app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/playground/recycle.kt
@@ -1,6 +1,16 @@
 package com.lukaslechner.coroutineusecasesonandroid.playground
 
-import java.util.PriorityQueue
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Delay
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.ensureActive
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.yield
+import kotlin.coroutines.EmptyCoroutineContext
 
 class TreeNode(var `val`: Int) {
     var left: TreeNode? = null
@@ -15,10 +25,66 @@ fun maxLevelSum(root: TreeNode?): Int {
 
 fun dfs(root: TreeNode?, level: Int) {
     if (root == null) return
-    if (list.size < level + 1){
+    if (list.size < level + 1) {
         list.add(0)
     }
-    list[level]+= root.`val`
+    list[level] += root.`val`
     dfs(root.left, level + 1)
     dfs(root.right, level + 1)
+}
+
+
+fun maisdasdn() {
+    val flow = flow {
+        emit(1)
+        delay(100)
+        emit(2)
+        delay(100)
+        emit(3)
+    }
+
+    val scope = CoroutineScope(Job())
+    scope.launch {
+        flow.collect {
+            println("collecting $it")
+            println("....$it")
+        }
+    }
+}
+
+fun main() {
+//    val producer = MutableStateFlow<Int>(0)
+    val scope = CoroutineScope(EmptyCoroutineContext)
+    scope.launch {
+        for (i in 1..4) {
+            println("emitting $i")
+            itemEmitted(i, this)
+            delay(100)
+        }
+    }
+    Thread.sleep(5000)
+}
+
+val delay = 500L
+var job: Job? = null
+
+fun itemEmitted(
+    item: Int,
+    scope: CoroutineScope
+) {
+    job?.cancel()
+    job = null
+    job = fireAfterDebounce(scope, item)
+}
+
+fun fireAfterDebounce(scope: CoroutineScope, item: Int?): Job {
+    val job = scope.launch(Dispatchers.IO) {
+        delay(delay)
+        ensureActive()
+        println("fired with $item with Debounced ")
+    }
+    job.invokeOnCompletion {
+        println("job.invokeOnCompletion $item :- $it ")
+    }
+    return job
 }
